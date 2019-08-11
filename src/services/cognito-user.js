3386f0227764b75302aaee1f1f675b170a65b565
Kieran Derfus <kieranderfus@gmail.com>
2019-08-11 18:25:23 -0400

    CLEAN: remove dead code

--- src/services/cognito-user.js
+++ src/services/cognito-user.js
@@ -4,15 +4,8 @@ import { notification } from 'antd'
 Amplify.configure({
   Auth: {
 
-    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
-    // identityPoolId: 'us-east-2_2hwpEnfaj',
-
     // REQUIRED - Amazon Cognito Region
     region: 'us-east-2',
-
-    // OPTIONAL - Amazon Cognito Federated Identity Pool Region
-    // Required only if it's different from Amazon Cognito Region
-    // identityPoolRegion: 'XX-XXXX-X',
 
     // OPTIONAL - Amazon Cognito User Pool ID
     userPoolId: 'us-east-2_2hwpEnfaj',
@@ -22,53 +15,8 @@ Amplify.configure({
 
     // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
     mandatorySignIn: true,
-
-    // OPTIONAL - Configuration for cookie storage
-    // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
-    // cookieStorage: {
-    //   // REQUIRED - Cookie domain (only required if cookieStorage is provided)
-    //   domain: '.yourdomain.com',
-    //   // OPTIONAL - Cookie path
-    //   path: '/',
-    //   // OPTIONAL - Cookie expiration in days
-    //   expires: 365,
-    //   // OPTIONAL - Cookie secure flag
-    //   // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
-    //   secure: true
-    // },
-    //
-    // // OPTIONAL - customized storage object
-    // storage: new MyStorage(),
-    //
-    // // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-    // authenticationFlowType: 'USER_PASSWORD_AUTH'
   }
 });
-
-// export async function login(email, password) {
-//   return firebaseAuth()
-//     .signInWithEmailAndPassword(email, password)
-//     .then(() => true)
-//     .catch(error => {
-//       notification.warning({
-//         message: error.code,
-//         description: error.message,
-//       })
-//     })
-// }
-
-// export async function login(email, password) {
-//   try {
-//     // const user = await Auth.signIn(username, password);
-//     const user = await Auth.signIn(email, password);
-//     console.log("USER: ", user)
-//   } catch (error) {
-//     notification.warning({
-//         message: error.code,
-//         description: error.message,
-//       })
-//   }
-// }
 
 export async function login(email, password) {
   return Auth.signIn(email, password)
@@ -90,6 +38,3 @@ export async function logout() {
     .then(() => true)
     .catch(err => console.log(err));
 }
-
-// You can get the current config object
-// const currentConfig = Auth.configure();
