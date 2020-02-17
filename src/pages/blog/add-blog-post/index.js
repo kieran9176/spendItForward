90a4fa8f665009361386ea3698d37e7c458f459f
Kieran Derfus <kieranderfus@gmail.com>
2020-02-17 17:25:23 -0500

    EDIT: Consolidate editor files

--- src/pages/blog/add-blog-post/index.js
+++ src/pages/blog/add-blog-post/index.js
@@ -2,17 +2,178 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 import { injectIntl } from 'react-intl'
-import { Form } from 'antd'
-import SimpleStaticToolbarEditor from '../../../components/Editor/SimpleToolbarEditor'
-import styles from './style.module.scss'
+import { Row, Col, Card, Divider } from 'antd'
+import { convertToRaw, EditorState, ContentState } from 'draft-js'
+import htmlToDraft from 'html-to-draftjs'
+import draftToMarkdown from 'draftjs-to-markdown'
+import draftToHtml from 'draftjs-to-html'
+import Editor from 'draft-js-plugins-editor'
+import createToolbarPlugin from 'draft-js-static-toolbar-plugin'
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  CodeBlockButton,
+} from 'draft-js-buttons'
+import './styles/draft.css'
+import './styles/plugin.css'
+import editorStyles from './styles/editorStyles.css'
+import styles from './styles/style.module.scss'
+
+const toolbarPlugin = createToolbarPlugin()
+const { Toolbar } = toolbarPlugin
+const plugins = [toolbarPlugin]
+
+const gridStyle = {
+  width: '10%',
+}
+
+class ToolbarComponent extends React.Component {
+  render() {
+    return (
+      <Row type="flex" justify="space-between" align="bottom">
+        <Col>
+          <Divider dashed style={{ width: 850 }} />
+          <Toolbar>
+            {externalProps => (
+              <Card size="small" style={{ width: 850, height: 68 }}>
+                <Card.Grid style={gridStyle}>
+                  <BoldButton {...externalProps} />
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <ItalicButton {...externalProps} />
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <UnderlineButton {...externalProps} />
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <CodeButton {...externalProps} />
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <UnorderedListButton {...externalProps} />
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <OrderedListButton {...externalProps} />
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <CodeBlockButton {...externalProps} />
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <HeadlineOneButton {...externalProps} />
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <HeadlineTwoButton {...externalProps} />
+                </Card.Grid>
+                <Card.Grid style={gridStyle}>
+                  <HeadlineThreeButton {...externalProps} />
+                </Card.Grid>
+              </Card>
+            )}
+          </Toolbar>
+        </Col>
+      </Row>
+    )
+  }
+}
 
 @withRouter
-@Form.create()
 @connect(({ profile }) => ({ profile }))
 @injectIntl
 class BlogAddPost extends React.Component {
-  state = {
-    showSearch: true,
+  constructor(props) {
+    super(props)
+    const contentBlock = htmlToDraft('<p>Let&apos;s hear it.</p>')
+    if (contentBlock) {
+      const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks)
+      const editorState = EditorState.createWithContent(contentState)
+      this.state = {
+        editorState,
+        showSearch: true,
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.setFormState()
+  }
+
+  setFormState = () => {
+    const { profile } = this.props
+    const { posts, currentPost } = profile
+    const { id } = currentPost
+    let post = {}
+
+    if (id) {
+      ;[post] = posts.filter(postObj => postObj.id === id)
+      const { html, markdown, url, title } = post
+      const contentBlock = htmlToDraft(html)
+      if (contentBlock) {
+        const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks)
+        const editorState = EditorState.createWithContent(contentState)
+        this.setState({
+          editorState,
+          title,
+          html,
+          markdown,
+          id,
+          url,
+        })
+      }
+    } else {
+      this.setState(post)
+    }
+  }
+
+  convertToReadable = editorState => {
+    const rawContentState = convertToRaw(editorState.getCurrentContent())
+    const markdown = draftToMarkdown(rawContentState)
+    const html = draftToHtml(rawContentState)
+
+    return { html, markdown }
+  }
+
+  decideToDispatch = state => {
+    const { dispatch } = this.props
+    if (state.html) {
+      dispatch({
+        type: 'profile/EDIT_POST_LOCALLY',
+        payload: state,
+      })
+      dispatch({
+        type: 'profile/CURRENT_POST',
+        payload: { saved: 'false' },
+      })
+    }
+  }
+
+  onChange = (changeType, e) => {
+    if (changeType === 'title') {
+      e.preventDefault()
+      this.setState({
+        title: e.target.value,
+      })
+      this.decideToDispatch(this.state)
+    }
+
+    if (changeType === 'editor') {
+      const { html, markdown } = this.convertToReadable(e)
+      this.setState({
+        editorState: e,
+        html,
+        markdown,
+      })
+      this.decideToDispatch(this.state)
+    }
+  }
+
+  focus = () => {
+    this.editor.focus()
   }
 
   hideLiveSearch = () => {
@@ -22,8 +183,8 @@ class BlogAddPost extends React.Component {
   }
 
   render() {
-    const { showSearch } = this.state
-    const { form, match, dispatch } = this.props
+    const { showSearch, editorState, title } = this.state
+    const { match, dispatch } = this.props
     const { params } = match
     const { id, status } = params
 
@@ -71,7 +232,38 @@ class BlogAddPost extends React.Component {
                   <img className={styles.logo} src="resources/images/logo.png" alt="" />
                 </div>
                 <div className={styles.bodyInput}>
-                  <SimpleStaticToolbarEditor form={form} match={match} />
+                  <div>
+                    <input
+                      type="title"
+                      className={styles.searchInput}
+                      id="titleInput"
+                      placeholder="Title"
+                      ref={this.handleNode}
+                      defaultValue={title}
+                      onChange={e => this.onChange('title', e)}
+                    />
+                    <div
+                      role="button"
+                      className={editorStyles.editor}
+                      onClick={this.focus}
+                      tabIndex={0}
+                      onKeyDown={this.focus}
+                    >
+                      <Row>
+                        <Col span={18}>
+                          <Editor
+                            editorState={editorState}
+                            onChange={editorStateObj => this.onChange('editor', editorStateObj)}
+                            plugins={plugins}
+                            ref={element => {
+                              this.editor = element
+                            }}
+                          />
+                        </Col>
+                      </Row>
+                      <ToolbarComponent />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
